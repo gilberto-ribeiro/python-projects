058240a94ae962d921c332433abbc39a7fe3cfff
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-21 16:19:28 -0300

    Implementing DataBR class

---
+++ alura/python_brasil_validacao_de_dados_no_padrao_nacional/datas_br.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+
+class DataBR:
+
+    def __init__(self):
+        self.__momento_cadastro = datetime.now()
+
+    def __str__(self):
+        return self.__momento_cadastro.strftime('%d/%m/%Y %H:%M')
+
+    @property
+    def mes(self):
+        meses = [
+            'janeiro', 'fevereiro', 'março', 'abril',
+            'maio', 'junho', 'julho', 'agosto',
+            'setembro', 'outubro', 'novembro', 'dezembro'
+        ]
+        return meses[self.__momento_cadastro.month - 1]
+    
+    @property
+    def dia(self):
+        return self.__momento_cadastro.day
+    
+    @property
+    def dia_da_semana(self):
+        dias_da_semana = [
+            'segunda-feira', 'terça-feira', 'quarta-feira',
+            'quinta-feira', 'sexta-feira', 'sábado', 'domingo'
+        ]
+        # dias_da_semana = [wd + '-feira' if i in range(5) else wd for i, wd in enumerate(dias_da_semana)]
+        return dias_da_semana[self.__momento_cadastro.weekday()]
--- alura/python_brasil_validacao_de_dados_no_padrao_nacional/main.py
+++ alura/python_brasil_validacao_de_dados_no_padrao_nacional/main.py
@@ -1,12 +1,9 @@
 from cpf_cnpj import Documento
 from telefones_br import TelefoneBR
-import re
+from datas_br import DataBR
 
-# doc = Documento.cria_documento('51423928000110')
-# doc = Documento.cria_documento('11507568614')
-# print(doc)
-# print(doc.tipo)
-# print(type(doc))
-
-num = TelefoneBR('5112997429157')
-print(num)
+cad = DataBR()
+print(cad)
+print(cad.dia)
+print(cad.mes)
+print(cad.dia_da_semana)
