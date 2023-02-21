ea1fc73b743ac4d8beae3aba05921dbdc725f3d4
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-21 16:37:41 -0300

    Updating DataBR class

--- alura/python_brasil_validacao_de_dados_no_padrao_nacional/datas_br.py
+++ alura/python_brasil_validacao_de_dados_no_padrao_nacional/datas_br.py
@@ -7,17 +7,8 @@ class DataBR:
         self.__momento_cadastro = datetime.now()
 
     def __str__(self):
-        return self.__momento_cadastro.strftime('%d/%m/%Y %H:%M')
+        return self.formata_data()
 
-    @property
-    def mes(self):
-        meses = [
-            'janeiro', 'fevereiro', 'março', 'abril',
-            'maio', 'junho', 'julho', 'agosto',
-            'setembro', 'outubro', 'novembro', 'dezembro'
-        ]
-        return meses[self.__momento_cadastro.month - 1]
-    
     @property
     def dia(self):
         return self.__momento_cadastro.day
@@ -30,3 +21,22 @@ class DataBR:
         ]
         # dias_da_semana = [wd + '-feira' if i in range(5) else wd for i, wd in enumerate(dias_da_semana)]
         return dias_da_semana[self.__momento_cadastro.weekday()]
+    
+    @property
+    def mes(self):
+        meses = [
+            'janeiro', 'fevereiro', 'março', 'abril',
+            'maio', 'junho', 'julho', 'agosto',
+            'setembro', 'outubro', 'novembro', 'dezembro'
+        ]
+        return meses[self.__momento_cadastro.month - 1]
+
+    @property
+    def ano(self):
+        return self.__momento_cadastro.year
+    
+    def formata_data(self):
+        return self.__momento_cadastro.strftime('%d/%m/%Y %H:%M')
+    
+    def tempo_cadastro(self):
+        return datetime.now() - self.__momento_cadastro
--- alura/python_brasil_validacao_de_dados_no_padrao_nacional/main.py
+++ alura/python_brasil_validacao_de_dados_no_padrao_nacional/main.py
@@ -2,8 +2,10 @@ from cpf_cnpj import Documento
 from telefones_br import TelefoneBR
 from datas_br import DataBR
 
+
 cad = DataBR()
 print(cad)
 print(cad.dia)
-print(cad.mes)
 print(cad.dia_da_semana)
+print(cad.mes)
+print(cad.ano)
