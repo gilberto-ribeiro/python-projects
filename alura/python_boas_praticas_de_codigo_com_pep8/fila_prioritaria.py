09b137d220c114cdd675159c67cddacb26d635cf
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-22 17:59:21 -0300

    Updating dependency injection

--- alura/python_boas_praticas_de_codigo_com_pep8/fila_prioritaria.py
+++ alura/python_boas_praticas_de_codigo_com_pep8/fila_prioritaria.py
@@ -1,14 +1,17 @@
 from typing import Union, List, Dict
 
 from fila_base import FilaBase
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 from constantes import CODIGO_PRIORITARIO
+
+ClassesEstatistica = Union[EstatisticaDetalhada, EstatisticaResumida]
 
 
 class FilaPrioritaria(FilaBase):
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'{CODIGO_PRIORITARIO}{self.codigo}'
 
-    def estatistica(self, dia: str, agencia: int, classe_estatistica) \
+    def estatistica(self, classe_estatistica: ClassesEstatistica) \
             -> Dict[str, Union[int, str, List[str]]]:
-        estatistica = classe_estatistica(dia, agencia)
-        return estatistica.retorna_estatistica(self.clientes_atendidos)
+        return classe_estatistica.retorna_estatistica(self.clientes_atendidos)
