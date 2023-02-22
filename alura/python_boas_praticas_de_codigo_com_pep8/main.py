1b4ecdae6be7017c196892ed2f4d0a254183eb6c
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-22 08:52:33 -0300

    Adding files

---
+++ alura/python_boas_praticas_de_codigo_com_pep8/main.py
@@ -0,0 +1,23 @@
+from fila_prioritaria import FilaPrioritaria
+
+fila_teste = FilaPrioritaria()
+
+fila_teste.atualiza_fila()
+print(fila_teste.fila)
+fila_teste.atualiza_fila()
+print(fila_teste.fila)
+fila_teste.atualiza_fila()
+print(fila_teste.fila)
+fila_teste.atualiza_fila()
+print(fila_teste.fila)
+print(fila_teste.chama_cliente(15))
+print(fila_teste.fila)
+print(fila_teste.chama_cliente(15))
+print(fila_teste.fila)
+fila_teste.atualiza_fila()
+print(fila_teste.fila)
+fila_teste.atualiza_fila()
+print(fila_teste.fila)
+print(fila_teste.chama_cliente(15))
+print(fila_teste.fila)
+print(fila_teste.estatistica('22/02/2023', 152))
