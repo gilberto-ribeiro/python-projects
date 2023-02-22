7710143d459449a2c68d0b50f2a31cbae34745d8
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-02-22 11:33:11 -0300

    Adding abstract methods to FilaBase

--- alura/python_boas_praticas_de_codigo_com_pep8/fila_base.py
+++ alura/python_boas_praticas_de_codigo_com_pep8/fila_base.py
@@ -1,4 +1,7 @@
-class FilaBase:
+from abc import ABCMeta, abstractmethod
+
+
+class FilaBase(metaclass=ABCMeta):
     codigo: int = 0
     fila: list = []
     clientes_atendidos: list = []
@@ -15,3 +18,11 @@ class FilaBase:
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
+
+    @abstractmethod
+    def gera_senha_atual(self):
+        pass
+
+    @abstractmethod
+    def chama_cliente(self, caixa):
+        pass
