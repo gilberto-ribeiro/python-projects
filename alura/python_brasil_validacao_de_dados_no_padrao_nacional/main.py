608998e6af8ec35b4359503c607716faa316801b
Gilberto Ribeiro Pinto Júnior <gilbertoribeirocvt@gmail.com>
2023-03-01 17:03:58 -0300

    Changing to UNICAMP address

--- alura/python_brasil_validacao_de_dados_no_padrao_nacional/main.py
+++ alura/python_brasil_validacao_de_dados_no_padrao_nacional/main.py
@@ -1,7 +1,7 @@
-from cpf_cnpj import Documento
-from telefones_br import TelefoneBR
-from datas_br import Cadastro
+# from cpf_cnpj import Documento
+# from telefones_br import TelefoneBR
+# from datas_br import Cadastro
 from acesso_cep import BuscaEndereco
 
-moradia = BuscaEndereco(12600210)
-print(moradia)
+unicamp = BuscaEndereco(13083872)
+print(unicamp)
